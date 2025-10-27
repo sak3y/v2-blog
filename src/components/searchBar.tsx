e9f35d93a84ac876efa5593fd2595845839ce0d6
Sakey <sheikh36ahmed@gmail.com>
2025-10-27 14:37:33 +0000

    search function added

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,27 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import { useQuery } from "../hooks/useQuery";
 
 const SearchBar = () => {
   const { query, setQuery } = useQuery();
-  // Redirect with search results
-  const handleSearch = () => {
-    console.log(query);
+  const navigate = useNavigate(); // Allows user to redirect
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (query.length < 2) return;
+
+    // Redirect url to new url with query in address
+    navigate(`/search?q=${encodeURIComponent(query)}`);
   };
   return (
-    <input
-      name="search"
-      type="text"
-      placeholder="Search..."
-      autoFocus
-      autoComplete="off"
-      value={query}
-      onChange={(e) => {
-        setQuery(e.target.value);
-      }}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") handleSearch();
-      }}
-      className="text-sm font-sans w-25 sm:w-50 px-1 border-1 focus:rounded-bl-none outline-0"
-    />
+    <form onSubmit={handleSearch}>
+      <input
+        name="search"
+        type="text"
+        placeholder="Search..."
+        autoFocus
+        autoComplete="off"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+        className="text-sm font-sans w-25 sm:w-50 px-1 border-1 focus:rounded-bl-none outline-0"
+      />
+    </form>
   );
 };
 
--- src/pages/searchResults.tsx
+++ src/pages/searchResults.tsx
@@ -1,9 +1,39 @@
-const searchResults = () => {
-    
-    return (<div className="flex flex-col gap-4 items-center">
+import { useLocation } from "react-router-dom";
+import Footer from "../components/footer";
+import { usePosts } from "../hooks/usePosts";
+import Post from "../components/post";
 
+const useQuery = () => new URLSearchParams(useLocation().search);
 
-    </div>)
-};
+function SearchResults() {
+  const posts = usePosts(); // Post data hook
 
-export default searchResults;
+  const query = useQuery().get("q")?.toLowerCase() || "";
+
+  const results = posts.posts.filter((item) => item.title.toLowerCase().includes(query));
+
+  return (
+    <div className="flex flex-col items-center max-w-[90vw] sm:max-w-[50vw] gap-4 mt-30">
+      <h2 className="text-5xl font-black text-center italic">Search Results for "{query}"</h2>
+
+      {/* If results are found */}
+      {results.length > 0 ? (
+        <ul className="flex flex-col gap-15 mt-10">
+          {results.map((post) => (
+            <Post
+              id={post.id}
+              title={post.title}
+              dateMade={post.dateMade}
+              description={post.description}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p className="text-xl font-semibold mt-10">No results found.</p>
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+export default SearchResults;
